abec231709df151060c177b60670103b6acfcd47
JenBrook <jenbrook375@hotmail.com>
2019-05-02 09:27:51 -0700

    added second accompanied check

--- js/operators.js
+++ js/operators.js
@@ -8,15 +8,18 @@ if (x > 18 && y > 21) {
     console.log('Too bad');
 
 // set age (int) set boolian accompaniedByAdult    
-var age = 17;
-var accompaniedByAdult = true;
+var age = 18;
+var accompaniedByAdult = false;
 // check if age falls within 18-21, 21 or over, or none of these, then display alert
 if (age >= 18 && age <= 21 && accompaniedByAdult == true) {
     alert('Allowed!');
+}else if(age>= 18 && age <= 21 && accompaniedByAdult == false){
+    alert('Denied!');
 }else if (age >= 21){
     alert('You are free!');
 }else {
     alert('Try again next year!');
 } 
+ 
 
 
