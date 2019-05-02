10658e129f7669ed76aff8a7ceeafcff1342a0eb
JenBrook <jenbrook375@hotmail.com>
2019-05-02 11:42:05 -0700

    creating userName prompt and checking for correct input

---
+++ js/review.js
@@ -0,0 +1,9 @@
+// empty object
+var userName = prompt("Enter Name");
+
+if (userName.valueOf.length == 0 || userName == ' ' || userName != NaN){
+    alert('Please enter a name');
+}else { if(userName.valueOf.length > 0 && userName != ' ' && userName == NaN){
+    console.log(userName);
+}
+}
